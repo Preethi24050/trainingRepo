eec81733fb0d8e27788933a143bd364d5df7730b
Preethi <preethi@DESKTOP-7N9F8QC.(none)>
2021-11-12 13:03:35 +0530

    remote

---
+++ hybris/custom/training/traininginitialdata/gensrc/com/hybris/training/initialdata/constants/GeneratedTrainingInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Nov 11, 2021, 5:40:32 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package com.hybris.training.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "traininginitialdata";
+	
+	protected GeneratedTrainingInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ hybris/custom/training/trainingtest/gensrc/com/hybris/training/test/constants/GeneratedTrainingTestConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Nov 11, 2021, 5:40:32 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package com.hybris.training.test.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingTestConstants
+{
+	public static final String EXTENSIONNAME = "trainingtest";
+	
+	protected GeneratedTrainingTestConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
